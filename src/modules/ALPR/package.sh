49688c9d1146a28249721fa43818001aca99f680
Chris Maunder <chris.maunder@codeproject.com>
2024-04-26 08:38:52 -0400

    v2.7: Modules have been pulled out into separate repos and use the PyPi / Nuget SDKs

--- src/modules/ALPR/package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Module Packaging script. To be called from create_packages.sh
-
-moduleId=$1
-version=$2
-
-tar -caf ${moduleId}-${version}.zip --exclude=__pycache__  --exclude=*.development.* --exclude=*.log \
-    patch/* utils/* *.py modulesettings.* requirements.* install.sh install.bat post_install.sh post_install.bat explore.html
--- src/modules/ObjectDetectionCoral/package.bat
+++
@@ -1,9 +0,0 @@
-@Echo off
-REM Module Packaging script. To be called from create_packages.bat
-
-set moduleId=%~1
-set version=%~2
-
-tar -caf %moduleId%-%version%.zip --exclude=__pycache__  --exclude=edgetpu_runtime --exclude=*.development.* --exclude=*.log ^
-    *.py modulesettings.* requirements.* install.sh install.bat explore.html test\* 
-REM     pycoral\*
