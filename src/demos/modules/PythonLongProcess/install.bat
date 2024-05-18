a0f4186c008c5a8b124dd32c1be8ed7388d335b2
Chris Maunder <chris.maunder@codeproject.com>
2024-05-18 16:22:36 -0400

    Docs / categorisation updates

--- src/demos/modules/PythonLongProcess/install.bat
+++ src/demos/modules/PythonLongProcess/install.bat
@@ -2,13 +2,13 @@
 ::
 ::                           PythonLongProcess
 ::
-:: This script is only called using ..\..\src\setup.bat
+:: This script is only called using ..\..\..\src\setup.bat
 ::
 :: For help with install scripts, notes on variables and methods available, tips,
 :: and explanations, see /src/modules/install_script_help.md
 
 @if "%1" NEQ "install" (
-    echo This script is only called from ..\..\src\setup.bat
+    echo This script is only called from ..\..\..\src\setup.bat
     @pause
     @goto:eof
 )
--- src/demos/modules/PythonSimple/install.bat
+++ src/demos/modules/PythonSimple/install.bat
@@ -2,13 +2,13 @@
 ::
 ::                           Object Detection (YOLOv8)
 ::
-:: This script is only called from ..\..\setup.bat 
+:: This script is only called from ..\..\..\setup.bat 
 ::
 :: For help with install scripts, notes on variables and methods available, tips,
 :: and explanations, see /src/modules/install_script_help.md
 
 @if "%1" NEQ "install" (
-    echo This script is only called from ..\..\setup.bat
+    echo This script is only called from ..\..\..\setup.bat
     @pause
     @goto:eof
 )
